f60270ba3d8c595bc42beee7dd2a8f133de399ac
BraianCardozo7 <braiane.cardozo@gmail.com>
2019-09-13 16:37:31 -0300

    sE SUBEN CAMBIOS

--- src/app/clases/juego-agilidad.ts
+++ src/app/clases/juego-agilidad.ts
@@ -1,2 +1,4 @@
 export class JuegoAgilidad {
+    gano: boolean;
+    numeroIngresado: number;
 }
--- src/app/componentes/mapa-de-google/mapa-de-google.component.ts
+++ src/app/componentes/mapa-de-google/mapa-de-google.component.ts
@@ -19,7 +19,7 @@ export class MapaDeGoogleComponent implements OnInit {
   
   ngOnInit() {
   }
-
+mapClicked($event){}
 
   
 
